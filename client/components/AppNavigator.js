cda2348f84a1decbfa9f22f0b84cfc9c39013b31
Kemi Thomas <kemothom71@gmail.com>
2019-08-17 13:51:22 -0500

    merge

--- client/components/AppNavigator.js
+++ client/components/AppNavigator.js
@@ -2,11 +2,12 @@ import { createAppContainer, createStackNavigator } from 'react-navigation';
 import Home from './Home';
 import Friends from './Friends';
 import Login from './Login';
-
+import AllMessages from './AllMessages';
 const AppNavigator = createStackNavigator({
   Home: { screen: Home },
   Friends: { screen: Friends },
-  Login: { screen: Login }
+  Login: { screen: Login },
+  AllMessages: { screen: AllMessages }
 });
 
 const AppContainer = createAppContainer(AppNavigator);
